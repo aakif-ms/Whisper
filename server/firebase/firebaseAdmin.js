1236317f39f98130ef1d1fd0a9c40f61c27bb2dd
aakif-ms <aakif.msiddiqui@gmail.com>
2025-05-20 20:38:55 +0530

    add secret file

--- server/firebase/firebaseAdmin.js
+++ server/firebase/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-admin-sdk.json');
+const serviceAccount = require('/etc/secrets/firebase-admin-sdk.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
